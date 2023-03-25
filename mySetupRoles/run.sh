524eb438e27e7b4a90035f8eb8c24e2e262710a8
ibrahim <ibrahim@virthost01>
2023-03-25 21:10:14 +0100

    Sat Mar 25 21:10:14 CET 2023

--- mySetupRoles/run.sh
+++ mySetupRoles/run.sh
@@ -2,7 +2,7 @@
 #
 # Multi-server tests for the orchestration example.
 #set -e
-
+ansible-inventory --list
 # Other commands from the book.
 #ansible multi -b -m apt -a "name=chrony state=present"
 #ansible multi -b -K -m apt -a "name=chrony state=present"
@@ -24,7 +24,8 @@
 # ansible-playbook -b -c local main.yml
 # ansible-playbook bashrc.yml # works
 # ansible-playbook ansible.yml # works
-ansible-playbook --vault-id ~/.ssh/.ansible-vault.pw update.yml # works
+#ansible-playbook --vault-id ~/.ssh/.ansible-vault.pw update.yml # works
+ansible-playbook update.yml # works
 #ansible-playbook -bK test.yml
 #ansible-playbook -b main.yml
 #ansible-playbook -b --list-host main.yml
