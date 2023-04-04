8a77359c2fabfe82580d337f0404570f81f9560e
ibrahim <ibrahim@virthost01.skyline.lan>
2023-04-04 14:22:15 +0200

    Tue Apr  4 14:22:15 CEST 2023

---
+++ mySetupRoles/crontab/files/root/ansible.sh
@@ -0,0 +1,6 @@
+mkdir -p /home/ibrahim/sandbox-ansible/
+cd /home/ibrahim/sandbox-ansible/
+git clone git@github.com:ichibsah/ansible-for-devops.git
+cd /home/ibrahim/sandbox-ansible/ansible-for-devops/
+bash ./md-shop.sh
+#rm -rf /home/ibrahim/sandbox-ansible/
