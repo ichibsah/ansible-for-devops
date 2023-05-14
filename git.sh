7e06597c56e16f9bafa8423a939e76f75a770637
ibrahim <ibrahim@virthost01.skyline.lan>
2023-05-14 08:39:53 +0200

    Sun May 14 08:39:53 CEST 2023

--- git.sh
+++ git.sh
@@ -1,11 +1,16 @@
 
-git remote add master git@github.com:ichibsah/ansible-for-devops.git
+#git remote add master git@github.com:ichibsah/ansible-for-devops.git
 
-git push --force --set-upstream master master
+#git push --force --set-upstream master master
 
 # https://github.com/ichibsah/ansible-for-devops.git
 # git@github.com:ichibsah/ansible-for-devops.git
 #git push <name>
+
+#…or push an existing repository from the command line
+git remote add origin git@github.com:ichibsah/ansible-for-devops.git
+git branch -M main
+git push -u origin main
 
 
 git add .
