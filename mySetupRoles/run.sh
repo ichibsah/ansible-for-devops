7e3985d06ac36fa74c133f8d2effef47d8a87204
ibrahim <ibrahim@virthost01.skyline.lan>
2023-06-16 14:26:26 +0200

    Fri Jun 16 14:26:26 CEST 2023

--- mySetupRoles/run.sh
+++ mySetupRoles/run.sh
@@ -1,4 +1,7 @@
 docker build -t docker-agent-ansible:latest . 
+docker image tag docker-agent-ansible:latest docker.io/michibsah/docker-agent-ansible:latest
+docker login -u michibsah
+docker push docker.io/michibsah/docker-agent-ansible:latest
 #
 ansible-inventory -y --list
 #
