8981fb7b106720aeb753e2379c63f10ccdbf9d48
ibrahim <ibrahim@virthost01.skyline.lan>
2023-07-16 11:01:55 +0200

    Sun Jul 16 11:01:55 CEST 2023

---
+++ mySetupRoles/firstimeonly.sh
@@ -0,0 +1,8 @@
+# docker build -t docker-agent-ansible:latest . 
+# docker image tag docker-agent-ansible:latest docker.io/michibsah/docker-agent-ansible:latest
+# docker login -u michibsah
+# docker push docker.io/michibsah/docker-agent-ansible:latest
+#
+ansible-inventory -y --list
+#
+ansible-playbook run-firstimeonly.yml # works
